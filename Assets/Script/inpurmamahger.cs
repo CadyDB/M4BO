5190437887e4c1d8c5e764d4048c8376c50d9a9d
shikanokonokonokokoshitantan <jakubszymanzki@gmail.com>
2026-06-02 07:52:12 +0200

    swing swing swing swing swing
    peek a bo!

--- Assets/Script/inpurmamahger.cs
+++ Assets/Script/inpurmamahger.cs
@@ -7,11 +7,12 @@ using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 using TMPro;
+using UnityEngine.UI;
 
 public class inpurmamahger : MonoBehaviour
 {
     private List<string> bumperTags = new List<string>();   //lijst met geraakte tags
-    private int scoreMultiplier = 1;
+public int comboMultiplier = 1;
     public static Action<int, int> OnScoreChange { get; internal set; }
     public static event Action<string, int> onBumperHit;
     float hitLineY = -3.72f;//link met scene line
@@ -19,18 +20,19 @@ public class inpurmamahger : MonoBehaviour
     public static int score = 0;
     public float perfectRange = 0.3f;
     public static inpurmamahger Instance;
+    public Image healthImage;
     
     public  TextMeshProUGUI scoreText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         scoreText = GameObject.FindWithTag("Score").GetComponent<TextMeshProUGUI>();
-        inpurmamahger.onBumperHit += CheckForCombo;
+
         
     }
     private void OnDisable()
     {
-        inpurmamahger.onBumperHit -= CheckForCombo;
+
         //stop met luisteren naar action event onBumperHit als scene herstart of game stopt             
     }
 
@@ -87,6 +89,9 @@ public class inpurmamahger : MonoBehaviour
             //CheckLane(3);
         }
         scoreText.text = inpurmamahger.score.ToString();
+        healthImage.fillAmount = health / 100f;
+        health = Mathf.Clamp(health, 0, 100);
+        comboMultiplier = Mathf.Min(comboMultiplier * 2, 16);
     }
     Note CheckLane(int lane)
     {
@@ -126,35 +131,28 @@ public class inpurmamahger : MonoBehaviour
             if (yPos >= -4.5f && yPos <= -3.5f)
             {
                 Debug.Log("HIT PERFECT");
+                comboMultiplier *= 2;
                 health += 5;
-                score += 30;
-
+                score += 30 * comboMultiplier;
                 Destroy(closestNote);
-
                 return closestNote.GetComponent<Note>();
             }
             if (yPos >= -3.5f && yPos <= -2f)
             {
                 Debug.Log("HIT EARLY");
-                health += 0;
-                score += 15;
-
+                score += 15 * comboMultiplier;
                 Destroy(closestNote);
-
                 return closestNote.GetComponent<Note>();
             }
         }
-
         Debug.Log("HIT MISS");
+        comboMultiplier = 1;
         health -= 5;
-
         if (health < 0)
         {
             Die();
         }
-        return null;
-        
-            
+        return null;        
     }
     private void Die()
     {
@@ -164,28 +162,7 @@ public class inpurmamahger : MonoBehaviour
     {
         health = Mathf.Clamp(health, 0, 100);
     }
-    private void CheckForCombo(string tag, int bumperValue)
-    {
-
-        Debug.Log("check combo");
-        bumperTags.Add(tag);                              
-        if (bumperTags.Count > 1)                      
-        {
-            if (bumperTags[bumperTags.Count - 2] == bumperTags[bumperTags.Count - 1])
-            {
-                scoreMultiplier++;
-            }
-            else
-            {
-                scoreMultiplier = 1;
-                bumperTags.Clear();
-            }
-        }                                                   
-        inpurmamahger.Instance.AddScore(bumperValue * scoreMultiplier);
-
-        //print score en multiplier in de console
-        Debug.Log($"Score: {score} || Multiplier: {scoreMultiplier}X");
-    }
+    
     public void AddScore(int amount)
     {
         score = score + amount;
